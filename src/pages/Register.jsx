b6cec66dddace3242bd2cc49da9db8a06c75b300
Naayart <jerusha328@gmail.com>
2025-06-19 07:41:21 -0700

    Register page works

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,26 @@
+import backgroundVideo from "../assets/videos/background-2.mp4";
+
 export default function Register() {
-    return(
-        <div></div>
-    );
+  return (
+    <>
+      <div className="min-h-screen flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 relative h-64 md:h-auto">
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="absolute top-0 left-0 w-full h-full object-cover z-0"
+          >
+            <source src={backgroundVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+
+          <div className="relative z-10 flex items-center justify-center h-full">
+            <h1 className="text-white text-4xl font-bold">Welcome</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
